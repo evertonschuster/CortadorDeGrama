b488a52fb25882b46592420a1309ffc066c873af
everton schuster <everton.schuster@hotmail.com>
2019-02-15 22:13:29 -0200

    Aula $

--- CortadorDeGrama/agente/AgenteLaberinto.cs
+++ CortadorDeGrama/agente/AgenteLaberinto.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using CortadorDeGrama.ambiente;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CortadorDeGrama.agente
 {
+
     public class AgenteLaberinto
     {
+        public Labirinto laberinto  { get; set; }
+
+        private MovimentoAgenteLaberinto movimento;
+
+        public AgenteLaberinto(Labirinto laberinto)
+        {
+            this.laberinto = laberinto;
+
+            this.movimento = MovimentoAgenteLaberinto.CIMA;
+        }
     }
 }
  
